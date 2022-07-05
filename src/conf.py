6fb17422bcf29b71322575c681298e1a401558ca
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2022-07-05 11:10:57 +0200

    Add HANGMAN_PATH to conf

--- src/conf.py
+++ src/conf.py
@@ -1,3 +1,6 @@
+# File with Hangman words
+HANGMAN_PATH = "./resources/hangman_words.txt"
+
 # Pattern used to validate input before calling eval().
 # Allowed chars: 0-9, *, /, +, -, space, (, ), and ^.
 PATTERN_CALC_VALIDATION = r"^[0-9+\-*\/^\(\)\. ]+$"
